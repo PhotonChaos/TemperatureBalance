1b45143f989c44abc917c8c1836e9349b4dcf544
Corin Gaiotto <corin.gaiotto@gmail.com>
2025-02-07 20:33:20 -0500

    Add main soundtrack, and interactive transitions between 3 track layers

---
+++ scripts/soundtrack_handler.gd
@@ -0,0 +1,13 @@
+extends AudioStreamPlayer
+
+# TODO: once player heat value is implemented, use signals to transition between tracks
+#       at positive, neutral, and negative heat
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
