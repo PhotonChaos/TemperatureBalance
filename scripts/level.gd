a2bd54604fa3f2c258e2cb11e579167fcf570b7b
Carbon <calumgcoppack@gmail.com>
2025-02-07 21:50:05 -0500

    Added level loading and controls

--- scripts/level.gd
+++ scripts/level.gd
@@ -5,10 +5,10 @@ signal win
 signal loss
 
 @export var level_name: String
-@export var player: Player
+@export var player_template: PackedScene
 
-@onready var level_tilemap = $LevelLayer as TileMap
-@onready var player_tilemap = $PlayerLayer as TileMap
+@onready var level_tilemap = $LevelLayer as TileMapLayer
+@onready var player_tilemap = $PlayerLayer as TileMapLayer
 
 # This is set on level startup by reading the tilemap
 var player_ref: Player
@@ -38,15 +38,31 @@ func _ready() -> void:
 	var valid = true
 	
 	if not is_instance_valid(player_tilemap):
-		print("[color=red]ERROR: Missing player as child object. Make sure the player node is named 'Player'![/color]")
+		print("ERROR: Missing player as child object. Make sure the player node is named 'Player'!")
 		valid = false
 	
 	if not is_instance_valid(level_tilemap):
-		print("[color=red]ERROR: Missing layout Tilemap child object. Make sure the map node is named 'Layout'![/color]")
+		print("ERROR: Missing layout Tilemap child object. Make sure the map node is named 'Layout'!")
 		valid = false
 	
 	if not valid:
 		return
+	
+	var player_start: Rect2i = player_tilemap.get_used_rect()
+	
+	if player_start.get_area() > 1:
+		print("ERROR: Too many player cells!")
+		return
+	elif player_start.get_area() < 1:
+		print("ERROR: No player cell specified!")
+		return
+
+	player_ref = player_template.instantiate()
+	add_child(player_ref)
+	
+	player_ref.position = Vector2(player_start.position.x + 1, player_start.position.y + 6)
+	
+	player_tilemap.hide()
 	
 	print("Loaded", level_name)
 
